ba1ac29acba0f3637c4d979beef6da5335556a42
João Tiago <jtiagodev@gmail.com>
2020-11-12 10:58:48 +0000

    sample(): KV worker sample

--- samples/stockmarket-kv-worker.js
+++ samples/stockmarket-kv-worker.js
@@ -1,69 +1,140 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  addEventListener('scheduled', event => {
-    event.waitUntil(refreshCache(event))
-  })
-  
-  async function refreshCache(event) {
-    const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
-    const keysToArray = keys.split(",");
-    for(let i = 0; i < keysToArray.length - 1; i++) { // ignore last entry
-        const baseURL = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=`;
-        const summaryProfile = await fetch(`${baseURL}summaryProfile`);
-        const summaryProfileJSON = await summaryProfile.json();
-              if (summaryProfileJSON.quoteSummary && summaryProfileJSON.quoteSummary.result && summaryProfileJSON.quoteSummary.result[0] && summaryProfileJSON.quoteSummary.result[0].summaryProfile) {
-                                  await MARKETWATCH_SYMBOLS.put(symbol, JSON.stringify(data)); // refreshes our data using our scheduler (weekdays 24:00)
-              }
-      }
+/**
+ * Listener for Incoming Requests
+ */
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+/**
+ * Listener for Scheduled Events
+ */
+addEventListener("scheduled", (event) => {
+  event.waitUntil(refreshCache(event));
+});
+
+/**
+ * Appends key to value
+ */
+async function updateMarketWatchKeys(symbol) {
+  const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
+  const updatedKeys = keys + symbol + ",";
+  await MARKETWATCH_SYMBOLS.put("KEYS", updatedKeys);
+};
+
+/**
+ * If KEYS isn't set yet, initialize it
+ */
+async function initMarketWatchKeys() {
+  const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
+  if (keys === null) {
+    await MARKETWATCH_SYMBOLS.put("KEYS", "");
   }
-  
-  /**
-   * 
-   * @param {Request} request
-   */
-  async function handleRequest(request) {
-    // Process POST
-    if(request.method == 'POST'){
-        try {
-          let body = await request.json();
-          const symbol = body.symbol;
-  
-          if (!symbol) {
-             return new Response(null, { status: 400, statusText: `Provide symbol on request body` });
-          }
-          // If KEYS isn't set yet, initialize it
-          const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
-          if (keys === null) {
-            await MARKETWATCH_SYMBOLS.put("KEYS", "");
-          }
-          // Reads cached value from KV
-          const value = await MARKETWATCH_SYMBOLS.get(symbol);
-          if (value === null) {
-              // get infos from service - rate limit 5 calls/sec
-              const baseURL = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=`;
-              const summaryProfile = await fetch(`${baseURL}summaryProfile`);
-              const summaryProfileJSON = await summaryProfile.json();
-              if (summaryProfileJSON.quoteSummary.error) {
-                  const { code, description } = summaryProfileJSON.quoteSummary.error;
-                  return new Response(null, { status: 400, statusText: description });
-              } else {
-                  // Stores (JSON) value on KV
-                  const data = summaryProfileJSON.quoteSummary.result[0].summaryProfile;
-                  await MARKETWATCH_SYMBOLS.put(symbol, JSON.stringify(data));
-                  const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
-                  const updatedKeys = keys + symbol + ",";
-                  await MARKETWATCH_SYMBOLS.put("KEYS", updatedKeys);
-                  return new Response(JSON.stringify(data), {status: 200})
-              }
-          } else {
-            return new Response(JSON.stringify(value), { status: 200 })
-          }
-        } catch (err) {
-          return new Response(null, {status: 500, statusText: err.message });
-        }
+};
+
+/**
+ * Work-around for .keys()
+ * @param {} KVObject
+ */
+async function getMarketWatchKeys() {
+  const keys = await MARKETWATCH_SYMBOLS.get("KEYS");
+  if (keys === null) {
+    await MARKETWATCH_SYMBOLS.put("KEYS", "");
+    return [];
+  }
+  const keysToArray = keys.split(",");
+  keysToArray.pop(); // removes last entry
+  return keysToArray;
+};
+
+async function getCache(key, value) {
+  await MARKETWATCH_SYMBOLS.put(key, value);
+};
+
+async function getCache(key) {
+  const value = MARKETWATCH_SYMBOLS.get(key);
+  return value;
+};
+
+async function getYahooStockInfo(symbol) {
+  const baseURL = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=`;
+  try {
+    const summaryProfile = await fetch(`${baseURL}summaryProfile`);
+    const summaryProfileJSON = await summaryProfile.json();
+    const data = summaryProfileJSON.quoteSummary.result[0].summaryProfile;
+    return data;
+  } catch (err) {
+    throw new Error(400);
+  }
+};
+
+/**
+ * Refreshes Cache Data with the Scheduler
+ * @param {*} event
+ */
+async function refreshCache(event) {
+  const KVKeys = await getMarketWatchKeys();
+  for (let i = 0; i < KVKeys.length; i++) {
+    const symbol = KVKeys[i];
+    try {
+      const data = await getYahooStockInfo(symbol);
+      await setCache(symbol, data);
+      return;
+    } catch (e) {}
+  }
+}
+
+/**
+ * Handles POST requests
+ * @param {} request 
+ */
+async function handlePostRequest(request) {
+  try {
+    let body = await request.json();
+    const symbol = body.symbol;
+
+    if (!symbol) {
+      return new Response(null, {
+        status: 400,
+        statusText: `Provide symbol on request body`,
+      });
     }
-    return new Response(`Request data from symbol (@Body) using POST`)
+    await initMarketWatchKeys();
+    // Reads cached value from KV
+    const value = await getCache(symbol);
+    let res;
+    if (value === null) {
+      const data = await getYahooStockInfo(symbol);
+      await setCache(symbol, JSON.stringify(data));
+      await updateMarketWatchKeys(symbol);
+      res = JSON.stringify(data);
+    } else {
+      res = JSON.stringify(value);
+    }
+    return new Response(res, { status: 200 });
+  } catch (err) {
+    return new Response(null, { status: 400 });
   }
-  
+}
+
+/**
+ * Fallback handler for other request methods
+ * @param {} request 
+ */
+async function handleDefaultRequest(request) {
+  return new Response(`Request data from symbol (@Body) using POST`);
+}
+
+/**
+ * Handles Requests
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  switch (request.method) {
+    case "POST":
+      return await handlePostRequest(request);
+      break;
+    default:
+      return await handleDefaultRequest(request);
+      break;
+  }
+}
